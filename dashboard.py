8c971aad140c2825683ef317ba75c39490ab8290
Peddi Varun Kumar <varun@Peddis-MacBook-Air.local>
2025-03-23 00:25:45 +0530

    Set Debug to False

--- dashboard.py
+++ dashboard.py
@@ -810,4 +810,4 @@ def update_overview_charts(selected_year):
 # -------------------------------------------------
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 10000))
-  app.run(host="0.0.0.0", port=port, debug=True)
+  app.run(host="0.0.0.0", port=port, debug=False)
